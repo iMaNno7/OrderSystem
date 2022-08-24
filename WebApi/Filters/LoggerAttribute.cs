fc3734a6bcee8d67c1c4adaa91ad81ad86a9e1a9
ImanNoferesti <iman.noferesti@yahoo.com>
2022-08-24 18:44:54 +0430

    add web api

---
+++ WebApi/Filters/LoggerAttribute.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApi.Filters
+{
+    public class LoggerAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            //
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
---
+++ WebApi/Program.cs
@@ -0,0 +1,21 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+
+app.MapControllers();
+app.Run();
+
+internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
